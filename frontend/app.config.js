a1310f87814f97089485fbd678a433346caeca5c
Jack <jacknguyen9605@gmail.com>
2026-04-11 22:23:01 +1000

    Add Expo UI SwiftUI iOS demo flow

---
+++ frontend/app.config.js
@@ -0,0 +1,36 @@
+const { expo } = require("./app.json");
+
+module.exports = ({ config }) => {
+  return {
+    ...config,
+    ...expo,
+    plugins: [
+      ...(expo.plugins ?? []),
+      "expo-dev-client",
+      ["expo-notifications", { enableBackgroundRemoteNotifications: false }],
+    ],
+    mods: {
+      ...config.mods,
+      ios: {
+        ...(config.mods?.ios ?? {}),
+        entitlements: async (config) => {
+          delete config.modResults["aps-environment"];
+          return config;
+        },
+        infoPlist: async (config) => {
+          if (Array.isArray(config.modResults.UIBackgroundModes)) {
+            config.modResults.UIBackgroundModes = config.modResults.UIBackgroundModes.filter(
+              (mode) => mode !== "remote-notification"
+            );
+
+            if (config.modResults.UIBackgroundModes.length === 0) {
+              delete config.modResults.UIBackgroundModes;
+            }
+          }
+
+          return config;
+        },
+      },
+    },
+  };
+};
